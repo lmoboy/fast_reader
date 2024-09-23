e914bde3f84366ff65fd0a858fb314f2b547a88e
lmoboy <steamgoyu19@gmail.com>
2024-09-23 19:54:25 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
+import "./App.css";
 
+  /**
+   * App renders a button that prompts the user to select a text file to read.
+   * When the user selects a file, the text is read and the words are displayed one
+   * at a time. The user can pause/resume the flow of words by pressing the space
+   * key. The display is updated every 100ms.
+   */
 function App() {
+  const [text, setText] = useState("");
+  const [wordIndex, setWordIndex] = useState(0);
+  const [paused, setPaused] = useState(false);
+  const timerRef = useRef(null);
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.key === " " && !paused) {
+        setPaused(true);
+      } else if (e.key === " " && paused) {
+        setPaused(false);
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [paused]);
+
+  useEffect(() => {
+    if (paused) {
+      clearInterval(timerRef.current);
+    } else {
+      timerRef.current = setInterval(() => {
+        if (wordIndex < text.split(" ").length) {
+          setWordIndex(wordIndex + 1);
+        }
+      }, 100);
+    }
+    return () => clearInterval(timerRef.current);
+  }, [wordIndex, text, paused]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        {text ? (
+          text.split(" ")[wordIndex]
+        ) : (
+          <button
+            onClick={() => {
+              const fileInput = document.createElement("input");
+              fileInput.type = "file";
+              fileInput.onchange = (e) => {
+                const file = e.target.files[0];
+                const reader = new FileReader();
+                reader.onload = () => {
+                  const text = reader.result
+                    .replace(/["']/g, "")
+                    .replace(/[-]/g, "")
+                    .replace(/[^\w\s]/gi, "");
+                  setText(text);
+                };
+                reader.readAsText(file);
+              };
+              fileInput.click();
+            }}
+          >
+            Press Me
+          </button>
+        )}
+      </h1>
     </div>
   );
 }
 
 export default App;
+
